8c53249ca0a04e64eb6f965f633d043dda7321f0
g-torres98 <lupitatorres632@gmail.com>
2019-11-16 14:45:31 -0600

    commit

---
+++ app/src/main/java/com/example/hogfeed/ViewModel/ViewModel.java
@@ -0,0 +1,17 @@
+package com.example.hogfeed.ViewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+
+//ViewModel will store and manage UI-related data
+public class ViewModel extends AndroidViewModel {
+
+    public ViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+}
+
